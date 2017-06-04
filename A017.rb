116c6510924b56de757e1af30e9c00dda6288006
yamakawa00 <leokun0210@gmail.com>
2017-06-04 07:19:54 +0000

    update

--- A017.rb
+++ A017.rb
@@ -9,20 +9,19 @@ def split_data(d)
 end
 
 H, W, N = read_header
-# generate filed
+# filed生成
 filed = Array.new(H).map{ Array.new(W, ".") } 
 
 while d = gets
+  start_y = 0
   # 読み込み
   h_i ,w_i ,x_i = split_data(d)
   
-  # y軸は上から参照する
+  # y軸は上から参照する(下から参照すると抜け漏れが発生するため)
   0.upto(H - 1) do |y|
     cnt = 0
     
     x_i.upto(x_i + w_i - 1) do |x|
-    # p "x " + x.to_s
-    # p "y " + y.to_s
       if filed[y][x] == "." then
         cnt += 1
       else
@@ -31,26 +30,23 @@ while d = gets
       end
     end
     
-    # '#'埋め処理
+    # '#'が出現する行を探索したか、yが底辺まで到達した場合、'#'埋め処理に入る
     if cnt != w_i || y + 1 == H
-      if cnt != w_i
-        y -= 1
+      if cnt != w_i then
+        start_y = y - 1
+      else
+        start_y = y
       end
-      y.downto(y - h_i + 1) do |yy|
-        x_i.upto(x_i + w_i - 1) do |x|
-          # p "insert"
-          # p "x " + x.to_s
-          # p "y " + y.to_s
-          filed[yy][x] = "#"
-          # filed.each { |f| puts f.join("")}
-        end
-      end
-      # p "insert break"
       break
     end
   end
-  # p "big loop break"
+  
+  # '#'埋め処理
+  start_y.downto(start_y - h_i + 1) do |yy|
+        x_i.upto(x_i + w_i - 1) do |x|
+          filed[yy][x] = "#"
+        end
+      end
 end
-filed.each { |f| puts f.join("")}
-exit()
 
+filed.each { |f| puts f.join("")}
