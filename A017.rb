c42c05d85bc288a66a78dc8b4154cac6c20e90fd
yamakawa00 <leokun0210@gmail.com>
2017-06-04 07:09:19 +0000

    update

--- A017.rb
+++ A017.rb
@@ -1,9 +1,5 @@
 # coding: utf-8
 
-def new_line
-  return "\n"
-end
-
 def read_header
   return gets.chomp.split(" ").map(&:to_i)
 end
@@ -12,19 +8,19 @@ def split_data(d)
   return d.chomp.split(" ").map(&:to_i)
 end
 
-TEST = ''
-
 H, W, N = read_header
 # generate filed
 filed = Array.new(H).map{ Array.new(W, ".") } 
 
 while d = gets
+  # 読み込み
   h_i ,w_i ,x_i = split_data(d)
-  # 処理
-  # for y in (H - 1)..0 do
+  
+  # y軸は上から参照する
   0.upto(H - 1) do |y|
     cnt = 0
-    for x in x_i..(x_i + w_i - 1) do
+    
+    x_i.upto(x_i + w_i - 1) do |x|
     # p "x " + x.to_s
     # p "y " + y.to_s
       if filed[y][x] == "." then
@@ -33,26 +29,21 @@ while d = gets
         cnt = 0
         break
       end
-      # if cnt != w_i
-      #   p "break"
-      #   y -= 1
-      #   break
-      # end
     end
-    # 場所確定後の#埋め処理
-    if cnt != w_i || y == H - 1
+    
+    # '#'埋め処理
+    if cnt != w_i || y + 1 == H
       if cnt != w_i
         y -= 1
       end
-      h_i.times do |h_p|
-        for x in x_i..(x_i + w_i - 1) do
+      y.downto(y - h_i + 1) do |yy|
+        x_i.upto(x_i + w_i - 1) do |x|
           # p "insert"
           # p "x " + x.to_s
           # p "y " + y.to_s
-          filed[y][x] = "#"
+          filed[yy][x] = "#"
           # filed.each { |f| puts f.join("")}
         end
-        y -= 1
       end
       # p "insert break"
       break
