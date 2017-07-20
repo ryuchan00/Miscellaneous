c0659ce558ea24e710e41b385a1dfff6315fdef5
yamakawa00 <leokun0210@gmail.com>
2017-07-20 15:08:24 +0000

    first commit

--- hoge.rb
+++ hoge.rb
@@ -1,17 +1,3 @@
-class Hoge
-    def initialize(x, y, z)
-        @x = x
-        @y = y
-        @z = z
-    end
-    
-    def display
-        p "x: " + @x.to_s
-        p "y: " + @y.to_s
-        p "x: " + @z.to_s
-    end
-end
-
-x, y, z = gets.split.map(&:to_i)
-hoge = Hoge.new(x, y, z)
-hoge.display
+array = [nil, 1, 2, 3, nil]
+# nilを削除して計算
+p array.compact.inject(&:+)
