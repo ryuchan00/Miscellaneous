d28d9024c0b4de6a4eb958f016cdaf2d20f6d57a
yamakawa00 <leokun0210@gmail.com>
2017-09-06 16:44:44 +0900

    add folder

---
+++ raspberry_pi/led_blink.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+while True:
+    GPIO.output(4, GPIO.HIGH)
+
+    time.sleep(1)
+
+    GPIO.outpu(4, GPIO.LOW)
+    time.sleep(1)
