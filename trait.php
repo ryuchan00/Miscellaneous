66d3aded05af9df119c5ed4c302d82c144c7e8b9
ryuchan00 <leokun0210@gmail.com>
2018-03-19 18:39:16 +0900

    traitの使い方がイマイチわからなかったので、動作確認

---
+++ trait.php
@@ -0,0 +1,22 @@
+<?php
+// [PHP: トレイト - Manual](http://php.net/manual/ja/language.oop5.traits.php)
+class Base {
+      public function sayHello() {
+                echo 'Hello ';
+                    }
+}
+
+trait SayWorld {
+      public function sayHello() {
+                parent::sayHello();
+                        echo 'World!';
+                            }
+}
+
+class MyHelloWorld extends Base {
+      use SayWorld;
+}
+
+$o = new MyHelloWorld();
+$o->sayHello();
+?>
