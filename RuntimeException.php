118b777a6c898f4af979eb8cf7f0ec6c4e33d5cd
ryuchan00 <leokun0210@gmail.com>
2018-03-23 17:36:24 +0900

    throwしたら、returnまで行かなよ

--- RuntimeException.php
+++ RuntimeException.php
@@ -4,16 +4,22 @@
 // throwの後の処理を実行するか忘れたので、実験
 $iNum1 = 10;
 $iNum2 = 0;
-#$iNum2 = 1;
-
-try{
-if ($iNum2 == 0){
-throw new RuntimeException("Division by Zero");
+# $iNum2 = 1;
+function throwError($iNum2) {
+  if ($iNum2 == 0){
+    throw new RuntimeException("Division by Zero");
+  }
+  $result = "function end";
+  return $result;
 }
+try{
+$response = throwError($iNum2);
+
 $iResult = $iNum1 / $iNum2;
-echo ("Division Result of \$iNum1 and $iNum2 = ".($iResult).PHP_EOF);
+echo ("Division Result of \$iNum1 and $iNum2 = ".($iResult));
 }
 catch (RuntimeException $e){
 echo ("Division by Zero is not possible");
 }
+echo $response;
 echo "after cathc block.";
