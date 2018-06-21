6a188e085997fe27b69b1876e7ee5945219312e6
ryuchan00 <leokun0210@gmail.com>
2018-06-21 11:22:34 +0900

    オプション引数について確認する必要があったため、検証した

--- ruby_verification/args/args2.rb
+++ ruby_verification/args/args2.rb
@@ -1,7 +1,16 @@
+# 可変長引数の挙動についての確認
 def greet(name, *messages)
   p messages #[[{:a=>1, :b=>2}], [3, 4]]
   p messages[0] # [{:a=>1, :b=>2}]
-
 end
 
-greet('name', [a: 1, b: 2], [3, 4])
+# greet('name', [a: 1, b: 2], [3, 4])
+
+# オプション引数の挙動についての確認
+def greet2(name, **messages)
+  p messages # {:id=>3, :type=>4}
+  p messages[:id] # 3
+  p messages[:no_args] # nil
+end
+
+greet2('name', id: 3, type: 4)
