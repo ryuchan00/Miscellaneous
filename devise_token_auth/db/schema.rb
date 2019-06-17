a6cb036b5ba18c905a8442947accb6999a2563f1
ryuchan00 <leokun0210@gmail.com>
2019-06-17 11:50:30 +0900

    このcolumnがないといけないのかー

--- devise_token_auth/db/schema.rb
+++ devise_token_auth/db/schema.rb
@@ -20,10 +20,11 @@ ActiveRecord::Schema.define(version: 2019_06_17_012542) do
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.string "nickname"
     t.string "image"
@@ -31,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_06_17_012542) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
